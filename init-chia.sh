f79d05ebeb7f19c1ebcde0e7c351a7ea563ebd75
Andrew Maier <amaier17@gmail.com>
2021-05-25 16:06:07 -0600

    init-chia: Refactor to remove sed command

--- init-chia.sh
+++ init-chia.sh
@@ -3,7 +3,8 @@
 . ./activate
 chia init -c /opt/main-ca/
 chia init
-sed -i 's/\*self_hostname/eid-chia-plotter-mk1/2' ~/.chia/mainnet/config/config.yaml
+chia configure --set-farmer-peer ec-full0:8447
+chia --log-level INFO
 
 chia plots add -d /plots
 chia start harvester
