b3a9c5e9e97eb337cc4422d8ed30a7b96ce3e13c
Andrew Maier <amaier17@gmail.com>
2021-08-13 10:57:10 -0600

    Added a start script
    This script will auto recursively look for plots based on the provided
    directory, build the docker, and start it.

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CA_PATH=${CA_PATH:-/home/amaier/main_ca}
+PLOT_PATH=${PLOT_PATH:-/mnt/plots}
+EC_NODE=${EC_NODE:-ec-full0}
+
+create_args() {
+	plot_folders=( `find $PLOT_PATH -name "*.plot" -type f -exec dirname {} \;` )
+	plot_folders=( `for i in "${plot_folders[@]}"; do echo $i; done | sort -u` )
+
+	mount_args=( "-v $CA_PATH:/opt/main_ca" )
+	run_args=( "$EC_NODE" )
+	index=0
+	for i in ${plot_folders[@]}; do
+		mount_args+=("-v $i:/d$index")
+		run_args+=("/d$index")
+		index=$((index+1))
+	done
+}
+create_args
+docker build --build-arg hostname=$HOSTNAME -t harvester:latest .
+docker run --name harvester0 -d --restart always ${mount_args[@]} -t harvester:latest ${run_args[@]}
+
