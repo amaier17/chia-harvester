1a885f707601684954ff7a525fc67c3fceb53d5e
David Sloan <delus.dlsloan@gmail.com>
2021-09-03 23:51:17 -0600

    added install script for prometheus server

---
+++ exporter/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+EXEC_PATH="$SCRIPT_PATH/exporter.py"
+
+cat > /etc/systemd/system/chia-harvester-exporter.service <<- EOM
+[Unit]
+Description=Prometheus exporter for chia harvesters
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+ExecStart=/usr/bin/env python3 $EXEC_PATH
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+systemctl daemon-reload
+systemctl enable chia-harvester-exporter.service
+systemctl start chia-harvester-exporter.service
